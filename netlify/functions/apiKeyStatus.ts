b3b66209fd989cc5b75947773a838f82be63161a
urgvard <urgvard@gmail.com>
2026-05-21 19:17:54 +0200

    debug: add env keys to apiKeyStatus

--- netlify/functions/apiKeyStatus.ts
+++ netlify/functions/apiKeyStatus.ts
@@ -4,7 +4,8 @@ export default async (req: Request, context: Context) => {
   const key = process.env.USER_GEMINI_API_KEY || process.env.GEMINI_API_KEY || "";
   const hasKey = !!key;
   const maskedKey = key ? `${key.substring(0, 6)}...${key.substring(key.length - 4)}` : "none";
-  return Response.json({ hasKey, maskedKey, nodeVersion: process.version });
+  const envKeys = Object.keys(process.env);
+  return Response.json({ hasKey, maskedKey, nodeVersion: process.version, envKeys });
 };
 
 export const config: Config = {
